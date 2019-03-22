1de338160b057f6e0d5933efbfbdf45490af5f61
Nathaniel Yaakov <swallowtailjig@gmail.com>
2019-03-22 04:43:59 -0400

    almost done!

--- src/event.js
+++ src/event.js
@@ -1,49 +1,38 @@
 var request;
-setTimeout( function(){
-  request = gapi.client.calendar.events.insert({
-    'calendarId': 'primary',
-    'resource': event
-  });
- }, 1000
-);
 
-var event = {
-  'id': 'googleio',
-  'summary': 'Google I/O 2019',
-  'location': '800 Howard St., San Francisco, CA 94103',
-  'description': 'A chance to hear more about Google\'s developer products.',
-  'start': {
-    'dateTime': '2019-03-18T09:00:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'end': {
-    'dateTime': '2019-03-18T17:00:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'recurrence': [
-    'RRULE:FREQ=DAILY;COUNT=2'
-  ],
-  'attendees': [
-    {'email': 'lpage@example.com'},
-    {'email': 'sbrin@example.com'}
-  ],
-  'reminders': {
-    'useDefault': false,
-    'overrides': [
-      {'method': 'email', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 10}
-    ]
-  }
-};
-eventId = event.id;
-console.log(eventId);
-function createCalEvent(){
-  console.log('Hello');
-  console.log(eventId);
-  request.execute(function() {
-    appendPre('Event created!');
-  });
+function eventCreation(rows) {
+  for (var i = 0; i < rows.length; i++) {
+
+      var event = {
+        'id': rows[i].SUBJECT_CODE + " " + rows[i].COURSE_NUMBER,
+        'summary': rows[i].COURSE_TITLE,
+        'location': '',
+        'description': rows[i].INSTRUCTOR + ", " + rows[i].METHOD + ", " + rows[i].TYPE ,
+        'start': {
+          'dateTime': '2019-03-18T' + rows[i].TIME_FROM.toString().substring(0, 2) + ":" + rows[i].TIME_FROM.toString().substring(2, rows[i].TIME_FROM.length) + ":00",
+          'timeZone': 'America/Los_Angeles'
+        },
+        'end': {
+          'dateTime': '2019-03-18T' + rows[i].TIME_FROM.toString().substring(0, 2) + ":" + rows[i].TIME_FROM.toString().substring(2, rows[i].TIME_FROM.length) + ":00",
+          'timeZone': 'America/Los_Angeles'
+        },
+        'recurrence': [
+          'RRULE:FREQ=WEEKLY;COUNT=2'
+        ]
+      };
+      eventId = event.id;    
+
+        request = gapi.client.calendar.events.insert({
+          'calendarId': 'primary',
+          'resource': event
+        });
+
+        request.execute(function() {
+          appendPre('Event created!');
+        });
+      }
 }
+
 
 function deleteCalEvent(eventId) {
     gapi.client.load('calendar', 'v3', function() {
--- src/main.js
+++ src/main.js
@@ -24,7 +24,6 @@ function generate(arr){
         return false;
     }
 
-    console.log(arr);
     var time = '';
     if ($("input[name='time']:checked").val() == "early") {
         time = '< 1800';
@@ -38,19 +37,18 @@ function generate(arr){
 
     classList += 'course_title = "' + arr[i].substring(0, arr[i].indexOf(':') - 1 ) + '"';
 
-    console.log(classList);
-
     $.ajax({
         type : "POST",
         url : "/create",
         dataType : "html",
         data : {times: time, classes : classList},
         success : function(rows) {
+            rows = JSON.parse(rows);
             console.log(rows);
+            eventCreation(rows);
         }, 
         error : function(jgxHR, textStatus, error) {
                 alert("Error: " + textStatus + " " + error); 
         }
     });
 }
-
