2a1a757e20dee7e4a82013135d5cb7efa9a7b07d
Nathaniel Yaakov <swallowtailjig@gmail.com>
2019-03-21 20:30:03 -0400

    search backend

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
-var express = require('express'); // must have express module
-var app = express();
-app.use(express.static('.')); // root directory of server is localhost:xxxx/
+var express = require("express");
+var app = express(); // starting server
+var bodyParser = require("body-parser");
 
-app.listen(3000);
+app.use(express.static("."));
+app.use(bodyParser.urlencoded({ extended: true })); // for encoded bodies
+
+
+var mysql = require("mysql");
+
+// setting up the database
+var con = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "Nathaniel1000",
+    database: "webtms"
+    });
+    con.connect(function(err) {
+    if (err) {
+    console.log("Error connecting to database");
+    }
+    else {
+    console.log("Database successfully connected");
+    }
+});
+
+app.post('/courses', function(req, res){
+    con.query('SELECT DISTINCT course_title FROM TMS', function(err, rows, fields) {
+        res.send(rows);
+    });
+});
+
+// listening to port 8080
+app.listen(8080, function() {
+    console.log("listening at port 8080....");
+});
---
+++ src/main.js
@@ -0,0 +1,17 @@
+var classes = [];
+
+
+function getClasses() {
+    
+}
+
+function getSections(current) {
+
+}
+
+function createCalendars() {
+    var calendars = [];
+    for (var i = 0; i < classes.length; i++) {
+        
+    }
+}
